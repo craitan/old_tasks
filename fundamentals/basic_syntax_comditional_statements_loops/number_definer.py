1a118ca94abbdbfb3b56a7d2dde5f3c79972dc5e
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-03-28 11:11:16 +0300

    task 1 lab

---
+++ fundamentals/basic_syntax_comditional_statements_loops/number_definer.py
@@ -0,0 +1,20 @@
+number = float(input())
+
+
+if number == 0:
+    print('zero')
+
+elif number > 0:
+    if number < 1:
+        print('small number')
+    elif number > 1000000:
+        print('large number')
+    else:
+        print('positive')
+else:
+    if abs(number) < 1:
+        print('small negative')
+    elif abs(number) > 1000000:
+        print('large negative')
+    else:
+        print('negative')

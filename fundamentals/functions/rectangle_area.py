c09d6c1a6fa1c979a5dc87dc2e74d325f62066e3
Yuliyan Petrov <104085417+craitan@users.noreply.github.com>
2023-04-11 12:34:25 +0300

    task 6 lab functions

---
+++ fundamentals/functions/rectangle_area.py
@@ -0,0 +1,16 @@
+# def rectangle(a, b):
+#     return a * b
+#
+#
+# width = int(input())
+# height = int(input())
+#
+# print(rectangle(width, height))
+
+
+result = lambda x, y: x * y
+
+width = int(input())
+height = int(input())
+
+print(result(width, height))
